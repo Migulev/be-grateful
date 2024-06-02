a1a1e0fa8fc9ba89a1018298ec2697f4f0dcb3d2
Anton Migulev <migulev@yandex.ru>
2024-06-02 12:28:46 -0400

    feat: logo add

---
+++ src/shared/ui/Logo.tsx
@@ -0,0 +1,11 @@
+export const Logo = () => {
+  return (
+    <img
+      src="src/shared/assets/icon.png"
+      alt="logo"
+      className=" size-10 md:size-12 lg:size-14"
+      width={60}
+      height={60}
+    />
+  )
+}
