c0bb7977561c0f07e1b35e95eed1e605c3683df8
Anton Migulev <migulev@yandex.ru>
2024-06-03 14:17:17 -0400

    refactor(theme-button): cleaning

--- src/features/theme/theme-button.tsx
+++ src/features/theme/theme-button.tsx
@@ -15,8 +15,6 @@ export const ThemeButton = () => {
     }
   }
 
-  const themIcon = theme === 'dark' ? 'moon' : 'sun'
-
   return (
     <>
       <UiButton
@@ -27,7 +25,6 @@ export const ThemeButton = () => {
           <>
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            {/* {themIcon} */}
           </>
         }
       </UiButton>
