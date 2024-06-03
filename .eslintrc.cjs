089024747f11bf98d7213212cb2b11d30882440f
Anton Migulev <migulev@yandex.ru>
2024-06-03 10:13:32 -0400

    shore(eslint): rules change

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,11 +11,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', 'prettier'],
   rules: {
+    'react-refresh/only-export-components': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
     'no-console': 'warn',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
   },
 }
