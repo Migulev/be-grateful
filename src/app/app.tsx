2202de84d052871710fa3665b89d242d2d2c1b28
Anton Migulev <migulev@yandex.ru>
2024-06-05 06:31:44 -0400

    feat: App component change

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,25 +1,19 @@
-import { ThemeButton } from '@/features/theme/theme-button'
 import { Home } from '@/pages/home'
-import { UiTopbar } from '@/shared/ui/layouts/ui-topbar'
-import { Logo } from '@/shared/ui/Logo'
+import { Topbar } from '@/widgets/topbar'
 
 import { AppLoader } from './app-loader'
 import { AppProviders } from './app-providers'
 
 function App() {
   return (
-    <div className="h-screen w-full bg-gradient-to-r from-cyan-500/90 to-blue-500/90 font-app">
-      <AppProviders>
-        <AppLoader>
-          <UiTopbar
-            logo={<Logo />}
-            theme={<ThemeButton />}
-            auth={<div>auth</div>}
-          />
+    <AppProviders>
+      <AppLoader>
+        <div className="h-screen w-full bg-gradient-to-r from-cyan-500/90 to-blue-500/90 font-app">
+          <Topbar />
           <Home />
-        </AppLoader>
-      </AppProviders>
-    </div>
+        </div>
+      </AppLoader>
+    </AppProviders>
   )
 }
 
