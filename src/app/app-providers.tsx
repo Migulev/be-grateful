da71f8ded0b58f2b6fe6a38c648a02367211595b
Anton Migulev <migulev@yandex.ru>
2024-06-10 15:22:31 -0400

    feat: query client provider add to app-provider

--- src/app/app-providers.tsx
+++ src/app/app-providers.tsx
@@ -1,10 +1,14 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+
 import { ThemeProvider } from '@/features/theme'
+import { queryClient } from '@/shared/libs/tanstack-query'
 import { ComposeChildren } from '@/shared/utils'
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
   return (
     <ComposeChildren>
       <ThemeProvider />
+      <QueryClientProvider client={queryClient} />
       {children}
     </ComposeChildren>
   )
