3b0f6b8a60fcce44def3c6ed6c63fa4e030aa0a7
Anton Migulev <migulev@yandex.ru>
2024-06-14 10:40:13 -0400

    refactor: app

--- src/app/app-providers.tsx
+++ src/app/app-providers.tsx
@@ -1,14 +1,17 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 
-import { ThemeProvider } from '@/features/theme'
 import { queryClient } from '@/shared/libs/tanstack-query'
 import { ComposeChildren } from '@/shared/utils'
+
+import { ThemeProvider } from './providers/theme-provider'
+import { ToastProvider } from './providers/toast-provider'
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
   return (
     <ComposeChildren>
-      <ThemeProvider />
       <QueryClientProvider client={queryClient} />
+      <ThemeProvider />
+      <ToastProvider />
       {children}
     </ComposeChildren>
   )
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,13 +5,15 @@ import { AppProviders } from './app-providers'
 
 function App() {
   return (
-    <AppProviders>
-      {/* !dev: color hardcode */}
-      <div className="h-screen w-full bg-gradient-to-r from-cyan-500/90 to-blue-500/90 font-app">
-        <Topbar />
-        <Home />
-      </div>
-    </AppProviders>
+    <>
+      <AppProviders>
+        {/* !dev: color hardcode */}
+        <div className="h-screen w-full bg-gradient-to-r from-cyan-500/90 to-blue-500/90 font-app">
+          <Topbar />
+          <Home />
+        </div>
+      </AppProviders>
+    </>
   )
 }
 
