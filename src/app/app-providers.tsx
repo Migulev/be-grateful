6a5c6abae34cf7fee038710a5398aaa079d0c03d
Anton Migulev <migulev@yandex.ru>
2024-06-17 18:05:39 -0400

    chore: tanstack devtools add

--- src/app/app-providers.tsx
+++ src/app/app-providers.tsx
@@ -1,4 +1,5 @@
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { queryClient } from '@/shared/libs/tanstack-query'
 import { ComposeChildren } from '@/shared/utils'
@@ -12,7 +13,9 @@ export function AppProviders({ children }: { children: React.ReactNode }) {
       <QueryClientProvider client={queryClient} />
       <ThemeProvider />
       <ToastProvider />
-      {children}
+      <>
+        {children} <ReactQueryDevtools initialIsOpen={false} />
+      </>
     </ComposeChildren>
   )
 }
