98165a59781806c8ebfe050d99281c749d3d3728
Anton Migulev <migulev@yandex.ru>
2024-06-21 09:17:14 -0400

    feat(theme-button): animation add

--- src/features/theme/ui/theme-button.tsx
+++ src/features/theme/ui/theme-button.tsx
@@ -15,18 +15,17 @@ export const ThemeButton = () => {
     }
   }
 
-  // !idea: receive component as a prop
   return (
     <>
       <Button
         size={'icon'}
         onClick={toggleTheme}
+        className="relative"
       >
         {
-          // !later: add animation
           <>
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <Sun className=" size-5 translate-x-0 opacity-100 [transition:opacity_200ms_ease-out,transform_300ms_ease-out]  dark:-translate-x-5 dark:opacity-0" />
+            <Moon className=" absolute size-5 translate-x-5 opacity-0 [transition:opacity_200ms_ease-out,transform_300ms_ease-out] dark:translate-x-0 dark:opacity-100" />
           </>
         }
       </Button>
