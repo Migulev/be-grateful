c47e6040b0058d420807f739374c74a809bc7590
Anton Migulev <migulev@yandex.ru>
2024-06-21 09:15:59 -0400

    chore: tailwind config cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,6 @@ module.exports = {
   ],
   prefix: '',
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
     extend: {
       fontFamily: {
         app: ['Rethink'],
