e619b4eda66e98e6cd03587baaa968e00e18cda0
Anton Migulev <migulev@yandex.ru>
2024-06-25 19:23:58 -0400

    refactor: auth modal

--- src/app/providers/modals/auth-modal-provider.tsx
+++ src/app/providers/modals/auth-modal-provider.tsx
@@ -1,15 +1,17 @@
 import { ReactNode, useState } from 'react'
 
+import { useSession } from '@/entities/session'
 import { AuthModalContext } from '@/shared/libs/modals/auth-modal-context'
 import { AuthModal } from '@/widgets/modals'
 
 export const AuthModalProvider = ({ children }: { children?: ReactNode }) => {
   const [isOpenAuthModal, setIsOpenAuthModal] = useState(false)
+  const session = useSession()
 
   return (
     <AuthModalContext.Provider value={{ setIsOpenAuthModal }}>
       {children}
-      {isOpenAuthModal && (
+      {isOpenAuthModal && !session && (
         <AuthModal onClose={() => setIsOpenAuthModal(false)} />
       )}
     </AuthModalContext.Provider>
