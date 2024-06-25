55c398a9ffbd39d04cc0d5f7313a36d457244a2d
Anton Migulev <migulev@yandex.ru>
2024-06-25 19:20:02 -0400

    refactor(features/auth): add exports

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,2 +1,4 @@
 export { SignInButton } from './ui/sign-in-button'
 export { useLogOut } from './model/use-log-out'
+export { useLoginWithOtp } from './model/use-log-in-with-otp'
+export { useLoginWithGoogle } from './model/use-log-in-with-google'
