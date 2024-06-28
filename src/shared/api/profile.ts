268bcb9a6aa48290c14a7cb173f5b178656fcd36
Anton Migulev <migulev@yandex.ru>
2024-06-28 18:57:12 -0400

    feat: profile api add

---
+++ src/shared/api/profile.ts
@@ -0,0 +1,26 @@
+import { supabase } from '@/shared/libs/supabase'
+
+export const profileApi = {
+  getAvatarStorageNameList: async (userId: string) => {
+    const { data, error } = await supabase.storage
+      .from('avatars')
+      .list(userId + '/')
+    if (error) throw new Error()
+
+    return data?.map(avatar => userId + '/' + avatar.name)
+  },
+
+  emptyAvatarStorage: async (avatarList: string[]) => {
+    const { error } = await supabase.storage.from('avatars').remove(avatarList)
+    if (error) throw new Error()
+  },
+
+  uploadAvatar: async (userId: string, file: File) => {
+    const { data, error } = await supabase.storage
+      .from('avatars')
+      .upload(userId + '/' + file.name, file)
+    if (error) throw new Error()
+
+    return data
+  },
+}
