39f29003ddd5c3d15644730da79ec92bf76cbdd3
Anton Migulev <migulev@yandex.ru>
2024-06-28 18:53:10 -0400

    refactor: gratitude query

--- src/entities/gratitude/model/types.ts
+++ src/entities/gratitude/model/types.ts
@@ -1,5 +1,12 @@
-export type Gratitude = {
-  id: string
-  title: string
-  createdAt: string
-}
+import { z } from 'zod'
+
+export const gratitudeSchema = z
+  .object({
+    id: z.string(),
+    title: z.string(),
+    created_at: z.string(),
+  })
+  .transform(({ created_at, ...rest }) => ({ createdAt: created_at, ...rest }))
+
+export const gratitudeArraySchema = z.array(gratitudeSchema)
+export type Gratitude = z.infer<typeof gratitudeSchema>
--- src/entities/gratitude/queries.ts
+++ src/entities/gratitude/queries.ts
@@ -2,11 +2,20 @@ import { useQueryClient } from '@tanstack/react-query'
 
 import { gratitudeApi } from '@/shared/api/gratitude'
 
+import { gratitudeArraySchema } from './model/types'
+
 export const gratitude_query_key = 'gratitudeList'
 
 export const gratitudeListQuery = () => ({
   queryKey: [gratitude_query_key],
-  queryFn: gratitudeApi.getGratitudeList,
+  queryFn: async () => {
+    const gratitudeList = await gratitudeApi.getGratitudeList()
+    const validation = gratitudeArraySchema.safeParse(gratitudeList)
+    if (validation.error) {
+      throw new Error()
+    }
+    return validation.data
+  },
   refetchOnWindowFocus: false,
   staleTime: Infinity,
 })
