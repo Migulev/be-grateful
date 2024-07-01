983ec46839572d050424735d97e25f4ffa4f71f2
Anton Migulev <migulev@yandex.ru>
2024-07-01 15:21:15 -0400

    feat(profile-avatar): spinner add

--- src/entities/profile/ui/profile-avatar.tsx
+++ src/entities/profile/ui/profile-avatar.tsx
@@ -1,3 +1,5 @@
+import { Loader } from 'lucide-react'
+
 import {
   Avatar,
   AvatarFallback,
@@ -11,21 +13,31 @@ import { getProfileLetters } from '../model/utils'
 export const ProfileAvatar = ({
   profile,
   className,
+  loading = false,
 }: {
   profile?: Profile
   className?: string
+  loading?: boolean
 }) => {
   if (!profile) {
     return null
   }
 
   return (
-    <Avatar className={cn(className)}>
+    <Avatar
+      className={cn('relative flex items-center justify-center', className)}
+    >
       <AvatarImage
         src={profile.avatarUrl ?? ''}
-        className="object-cover"
+        className={cn('object-cover', { 'opacity-20': loading })}
       />
-      <AvatarFallback>{getProfileLetters(profile)}</AvatarFallback>
+      <AvatarFallback className={cn({ 'opacity-20': loading })}>
+        {getProfileLetters(profile)}
+      </AvatarFallback>
+      {/* !dev: hardcoded color */}
+      {loading && (
+        <Loader className="animate-spin-slow absolute text-gray-900" />
+      )}
     </Avatar>
   )
 }
