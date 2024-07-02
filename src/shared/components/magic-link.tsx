541ba85a83c3e0d7bbfccd23b74e785ad2c92a5f
Anton Migulev <migulev@yandex.ru>
2024-07-01 21:05:50 -0400

    feat: magic-link

---
+++ src/shared/components/magic-link.tsx
@@ -0,0 +1,10 @@
+{
+  /* <div style="font-family: sans-serif; background-color: #309DE6; padding: 40px; border-radius: 10px">
+  <h2 style="color: #FFFFFF">Волшебная ссылка</h2>
+    <p style="color: #FFFFFF">
+      Авторизоваться на сайт "Благодарность на
+      каждый день".
+  </p>
+  <a style="color: #7ED3FC" href="{{ .ConfirmationURL }}">нажми меня</a>
+  </div> */
+}
