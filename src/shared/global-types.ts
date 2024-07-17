6eff59529a6307e8c01ba45b4ff02830485e0c01
Anton Migulev <migulev@yandex.ru>
2024-07-17 19:42:03 -0400

    refactor: type rename

--- src/shared/global-types.ts
+++ src/shared/global-types.ts
@@ -1,6 +1,6 @@
 export type DurationTW = 0 | 150 | 200 | 300 | 500 | 700 | 1000
 
-export type WithImageUrl<T> = T & {
+export type ObjectWithImageUrl<T> = T & {
   avatarUrl: string
   image?: HTMLImageElement
 }
