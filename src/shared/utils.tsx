c479611c2ed59026e62d6a44d2a01b08337fd035
Anton Migulev <migulev@yandex.ru>
2024-07-25 19:48:50 -0400

    refactor(global-types): folder structure + imports

--- src/shared/utils.tsx
+++ src/shared/utils.tsx
@@ -14,7 +14,7 @@ import {
 import { ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-import { ObjectWithImageUrl } from './config/global-types'
+import { ObjectWithImageUrl } from './global-types'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
