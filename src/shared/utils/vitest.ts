6f3d526da4af13a226e180e915260608b191d67c
Anton Migulev <migulev@yandex.ru>
2024-07-30 11:44:45 -0400

    refactor: utils/vitest

--- src/shared/utils/vitest.ts
+++ src/shared/utils/vitest.ts
@@ -1,5 +1,3 @@
-import { FormEvent } from 'react'
-
 export const customTextMatcher =
   (text: string) => (_: unknown, element: Element | null) => {
     const hasText = (node: Element | null) => node?.textContent === text
@@ -9,35 +7,3 @@ export const customTextMatcher =
     )
     return nodeHasText && childrenDontHaveText
   }
-
-export const mockEvent = {
-  preventDefault: vitest.fn(),
-  nativeEvent: {
-    bubbles: false,
-    cancelBubble: false,
-    cancelable: false,
-    composed: false,
-    currentTarget: {} as EventTarget,
-    defaultPrevented: false,
-    eventPhase: 0,
-    isTrusted: true,
-    returnValue: true,
-    srcElement: null,
-    target: {} as EventTarget,
-    timeStamp: Date.now(),
-    type: 'submit',
-  },
-  currentTarget: {} as EventTarget & HTMLFormElement,
-  target: {} as EventTarget,
-  bubbles: false,
-  cancelable: false,
-  defaultPrevented: false,
-  eventPhase: 0,
-  isTrusted: true,
-  stopPropagation: vitest.fn(),
-  isDefaultPrevented: vitest.fn(),
-  isPropagationStopped: vitest.fn(),
-  persist: vitest.fn(),
-  timeStamp: Date.now(),
-  type: 'submit',
-} as unknown as FormEvent<HTMLFormElement>
