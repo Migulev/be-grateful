201ec44746b8440341698360c4b79826b8d83ba3
Anton Migulev <migulev@yandex.ru>
2024-07-31 12:35:57 -0400

    chore: pwa + configuration + assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,38 @@ import path from 'path'
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const vitePWA = VitePWA({
+  registerType: 'autoUpdate',
+  outDir: 'dist',
+  manifest: {
+    name: 'gratitude note',
+    short_name: 'gratitude note',
+    description: 'simple gratitude note app',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: '/assets/pwa/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/assets/pwa/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+  workbox: {
+    globDirectory: 'dist/',
+    globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,svg}'],
+    swDest: 'dist/sw.js',
+  },
+})
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePWA],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
