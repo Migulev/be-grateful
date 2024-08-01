afa5ca8e4e1cfcf269547f603f6645ac7869f83c
Anton Migulev <migulev@yandex.ru>
2024-08-01 17:50:19 -0400

    refactor(drawer): padding bottom change to 40 px

--- src/shared/components/ui/drawer.tsx
+++ src/shared/components/ui/drawer.tsx
@@ -42,7 +42,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col gap-4 rounded-t-[10px] border bg-background pb-6',
+        'fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col gap-4 rounded-t-[10px] border bg-background pb-10',
         className,
       )}
       {...props}
