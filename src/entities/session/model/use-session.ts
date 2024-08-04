4aea0891bd04f88af9e660ef635ea17884a56f1b
Anton Migulev <migulev@yandex.ru>
2024-08-04 13:05:37 -0400

    refactor: use-session

--- src/entities/session/model/use-session.ts
+++ src/entities/session/model/use-session.ts
@@ -3,10 +3,10 @@ import { useQuery } from '@tanstack/react-query'
 import { sessionQuery } from '../queries'
 import { Session } from './types'
 
-export const useSession = (): Session => {
+export const useSession = (): Session | undefined => {
   const { data: session } = useQuery({
     ...sessionQuery(),
   })
 
-  return session as Session
+  return session
 }
