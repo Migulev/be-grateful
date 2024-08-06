79d1c32ac5e0236195aea6c4dadb3a3eb50d714e
Anton Migulev <migulev@yandex.ru>
2024-08-06 15:50:22 -0400

    bug(formatDate): fix

--- src/shared/utils/react.tsx
+++ src/shared/utils/react.tsx
@@ -61,8 +61,8 @@ export function convertDurationTW(duration: number) {
 }
 
 export const formatDate = (dateString: string) => {
-  const date = new Date(dateString)
-  const day = String(date.getDate() + 1).padStart(2, '0')
+  const date = new Date(dateString + 'T00:00:00')
+  const day = String(date.getDate()).padStart(2, '0')
   const month = String(date.getMonth() + 1).padStart(2, '0')
   const year = String(date.getFullYear()).slice(-2)
   return `${day}/${month}/${year}`
