cb612f01311a7d17e4d7947b6f921ad3531a2876
Anton Migulev <migulev@yandex.ru>
2024-08-16 09:11:51 -0400

    refactor(app-layout):  no scrollbar add

--- src/app/layouts/app-layout.tsx
+++ src/app/layouts/app-layout.tsx
@@ -7,7 +7,7 @@ import { GlobalSpinner } from '@/shared/components/global-spinner'
 
 export const AppLayout = () => {
   return (
-    <div className="bg-gradient relative h-screen overflow-auto font-app">
+    <div className="bg-gradient no-scrollbar relative h-screen overflow-auto font-app">
       <div className="container">
         <Topbar />
         <Suspense fallback={<GlobalSpinner />}>
