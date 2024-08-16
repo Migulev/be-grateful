af33c4f658614e8f2e716916bca13a3be26556b2
Anton Migulev <migulev@yandex.ru>
2024-08-15 20:49:21 -0400

    feat(app-layout): suspense add

--- src/app/layouts/app-layout.tsx
+++ src/app/layouts/app-layout.tsx
@@ -1,17 +1,19 @@
-import { Outlet, useNavigation } from 'react-router-dom'
+import { Suspense } from 'react'
+
+import { Outlet } from 'react-router-dom'
 
 import { Topbar } from '@/widgets/topbar'
 import { GlobalSpinner } from '@/shared/components/global-spinner'
 
 export const AppLayout = () => {
-  const navigation = useNavigation()
   return (
     <div className="bg-gradient relative h-screen overflow-auto font-app">
       <div className="container">
         <Topbar />
-        <Outlet />
+        <Suspense fallback={<GlobalSpinner />}>
+          <Outlet />
+        </Suspense>
       </div>
-      {navigation.state === 'loading' && <GlobalSpinner />}
     </div>
   )
 }
